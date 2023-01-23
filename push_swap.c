8ac94e0d3edc826a088e73dc7909f31708701e8e
Tiago Henrique Tavares <ttavares@c1r11s5.42porto.com>
2023-01-23 16:25:32 +0000

    update

--- push_swap.c
+++ push_swap.c
@@ -6,30 +6,104 @@
 /*   By: ttavares <ttavares@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/19 15:18:35 by ttavares          #+#    #+#             */
-/*   Updated: 2023/01/19 15:53:10 by ttavares         ###   ########.fr       */
+/*   Updated: 2023/01/23 16:10:16 by ttavares         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-typedef struct a_list {
-	int	val;
-	struct a_list * next;
-} a_list_t;
+void	printlist(t_stack *current_a)
+{
+	while (current_a != NULL)
+	{
+		ft_printf("%d\n", current_a->val);
+		current_a = current_a->next;
+	}
+}
 
-typedef struct b_list {
-	int val;
-	struct b_list * next;
-}b_list_t;
+void	insert_head(t_stack **head, int value)
+{
+	t_stack *new;
+
+	new = malloc(sizeof(t_stack));
+	new->val = value;
+	new->next = *head;
+	*head = new;
+}
+void	insert_tail(t_stack **head, int value)
+{
+	t_stack *new;
+	t_stack *curr;
+
+	curr = *head;
+	new = malloc(sizeof(t_stack));
+	if(!new)
+		exit(1);
+	new->next = NULL;
+	new->val = value;
+	if (*head == NULL)
+	{
+		*head = new;
+		return;
+	}
+	while(curr->next != NULL)
+		curr = curr->next;
+	curr->next = new;
+}
+
+void	insert_after(t_stack *prev, int value)
+{
+	t_stack *new;
+
+	if (prev == NULL)
+		return;
+	new = malloc(sizeof(t_stack));
+	if (!new)
+		exit (1);
+	new->val = value;
+	new->next = prev->next;
+	prev->next = new;
+}
+
+void	swap(t_stack **head)
+{
+	t_stack	*swap;
+
+	swap = (*head)->next;
+	(*head)->next = *head;
+	*head = swap;
+}
 
 int	main(int argc, char **argv)
 {
-	(void)argv;
+	t_stack	*head_a;
+	t_stack	*head_b;
+
+	int		i;
+	int		j;
+	head_a = NULL;
+	head_b = NULL;
 	if (argc == 1)
 		return (0);
-	if (argc != 2)
+	i = 1;
+	while (argv[i] != NULL)
 	{
-		ft_printf("Error\n");
+		j = 0;
+		while (argv[i][j] != '\0')
+		{
+			if (!ft_isdigit(argv[i][j]))
+				{
+					ft_printf("Error\n");
+					return (0);
+				}
+				j++;
+		}
+		insert_tail(&head_a, ft_atoi(argv[i]));
+		i++;
 	}
+	printlist(head_a);
+	ft_printf("A\n");
+	swap(&head_a);
+	printlist(head_a);
 	return (0);
 }
--- push_swap.h
+++ push_swap.h
@@ -6,7 +6,7 @@
 /*   By: ttavares <ttavares@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/19 10:31:42 by ttavares          #+#    #+#             */
-/*   Updated: 2023/01/19 15:25:50 by ttavares         ###   ########.fr       */
+/*   Updated: 2023/01/23 14:08:24 by ttavares         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,11 @@
 # define PUSH_SWAP_H
 
 # include "libft/libft.h"
+
+typedef struct s_stack
+{
+	int				val;
+	struct s_stack	*next;
+}	t_stack;
 
 #endif
