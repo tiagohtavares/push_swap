cfd5530b7b5674f093849e788b1d93eae2b33a22
Tiago Henrique Tavares <ttavares@c1r12s15.42porto.com>
2023-03-14 16:13:42 +0000

    test

--- push_swap.c
+++ push_swap.c
@@ -6,7 +6,7 @@
 /*   By: ttavares <ttavares@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/19 15:18:35 by ttavares          #+#    #+#             */
-/*   Updated: 2023/03/13 19:20:01 by ttavares         ###   ########.fr       */
+/*   Updated: 2023/03/14 15:55:50 by ttavares         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -252,6 +252,138 @@ void	sort_small(t_stack **head_a,t_stack **head_b, int size)
 	}
 }
 
+void	clear(t_stack **lst)
+{
+	t_stack *temp;
+
+	if (!lst)
+		return;
+	while (*lst != NULL)
+	{
+		temp = (*lst)->next;
+		free(*lst);
+		*lst = temp;
+	}
+}
+
+int	calc_moves(t_stack **head_a, t_stack **head_b, int maxb, int minb)
+{
+	t_stack *tempa;
+	t_stack *tempb;
+	t_stack *current;
+	t_stack *currenttest;
+	t_stack *currentx;
+	int		moves;
+	int		half;
+	int		i;
+	int		minmoves;
+	int		top;
+	int		num;
+	int		nextmin;
+
+	moves = 0;
+	minmoves = 0;
+	i = 0;
+	currenttest = *head_a;
+	tempa = NULL;
+	tempb = NULL;
+	top = (*head_a)->val;
+	if (stacklen(*head_a) % 2 != 0)
+		half = (stacklen(*head_a) / 2) + 1;
+	else
+		half = stacklen(*head_a) / 2;
+	while (i < stacklen(*head_a))
+	{
+		currentx = *head_a;
+		while (currentx != NULL)
+		{
+			insert_tail(&tempa,currentx->val);
+			currentx = currentx->next;
+		}
+		currentx = *head_b;
+		while (currentx != NULL)
+		{
+			insert_tail(&tempb,currentx->val);
+			currentx = currentx->next;
+
+		}
+		//ft_printf("LIST HAS BEEN COPIED \n");
+		//printlist(&tempa,&tempb);
+		if (i != 0)
+		{
+			currenttest = currenttest->next;
+			top = currenttest->val;
+		}
+		//ft_printf("TOP NUMBER %d \n",top);
+		while (top != tempa->val)
+		{
+			shiftup(&tempa,'x');
+			//printlist(&tempa,&tempb);
+			moves++;
+		}
+		if (tempa != NULL)
+		{
+			if (tempa->val > maxb || tempa->val < minb)
+			{
+				while (tempb->val != maxb)
+				{
+					shiftup(&tempb,'x');
+					//printlist(&tempa,&tempb);
+					moves++;
+				}
+				push(&tempa,&tempb,'x');
+				//printlist(&tempa,&tempb);
+				moves++;
+			}
+			else if (tempa->val > minb && tempa->val < maxb)
+			{
+				current = tempb;
+				nextmin = minb;
+				while (current != NULL)
+				{
+					if (tempa->val > current->val && nextmin < current->val)
+						nextmin = current->val;
+					current = current->next;
+				}
+				//ft_printf("Nextmin----TEEMP LIST = %d \n", nextmin);
+				while (tempb->val != nextmin)
+				{
+					shiftup(&tempb,'x');
+					moves++;
+					//printlist(&tempa,&tempb);
+				}
+				push(&tempa,&tempb,'x');
+				//printlist(&tempa,&tempb);
+				moves++;
+			}
+		}
+		if (minmoves == 0)
+		{
+			minmoves = moves;
+			num = tempb->val;
+		}
+		if (minmoves == 1 || minmoves == 2)
+		{
+			num = tempb->val;
+			//ft_printf("finished calculation!! nummoves is = %d, num is = %d\n",minmoves, num);
+			return (num);
+		}
+		else if (minmoves > moves)
+		{
+		minmoves = moves;
+		num = tempb->val;
+		}
+		i++;
+		//ft_printf("TEST %d \n" ,tempb->val);
+		//ft_printf("MOVES %d\n", moves);
+		//ft_printf("NUM %d\n",num);
+		clear(&tempa);
+		clear(&tempb);
+		//ft_printf("finished calculation!! nummoves is = %d, num is = %d\n",minmoves, num);
+	}
+	return (num);
+}
+
 void	sort_medium(t_stack **head_a,t_stack **head_b, int size)
 {
 	t_stack *current;
@@ -259,6 +391,7 @@ void	sort_medium(t_stack **head_a,t_stack **head_b, int size)
 	int minb;
 	int temp;
 	int nextmin;
+	int	top;
 
 	while (!list_is_sorted(head_a, size) && stacklen(*head_a) != 0)
 	{
@@ -266,14 +399,14 @@ void	sort_medium(t_stack **head_a,t_stack **head_b, int size)
 		{
 		maxb = (*head_a)->val;
 		push(head_a,head_b,'b');
-		printlist(head_a,head_b);
+		//printlist(head_a,head_b);
 		minb = (*head_a)->val;
 		push(head_a,head_b,'b');
-		printlist(head_a,head_b);
+		//printlist(head_a,head_b);
 		if (maxb > minb)
 		{
 			swap(head_b,'b');
-			printlist(head_a,head_b);
+			//printlist(head_a,head_b);
 		}
 		else if (maxb < minb)
 		{
@@ -282,24 +415,28 @@ void	sort_medium(t_stack **head_a,t_stack **head_b, int size)
 			minb = temp;
 		}
 		}
-		if ((*head_a)->val > maxb || (*head_a)->val < minb)
-		{
-			while ((*head_b)->val != maxb)
-			{
-				shiftup(head_b,'b');
-				printlist(head_a,head_b);
-			}
-			if ((*head_a)->val > maxb)
-				maxb = (*head_a)->val;
-			else if ((*head_a)->val < minb)
-				minb = (*head_a)->val;
-			push(head_a,head_b,'b');
-			printlist(head_a,head_b);
-
-		}
 		if (*head_a != NULL)
 		{
-			if ((*head_a)->val > minb && ((*head_a)->val < maxb))
+			current = *head_a;
+			//ft_printf("BEGGINING MOVE CALCULATIONS\n");
+			top = calc_moves(head_a, head_b, maxb, minb);
+			while ((*head_a)->val != top)
+				shiftup(head_a,'a');
+			if ((*head_a)->val > maxb || (*head_a)->val < minb)
+			{
+				while ((*head_b)->val != maxb)
+				{
+					shiftup(head_b,'b');
+					//printlist(head_a,head_b);
+				}
+				if ((*head_a)->val > maxb)
+					maxb = (*head_a)->val;
+				else if ((*head_a)->val < minb)
+					minb = (*head_a)->val;
+				push(head_a,head_b,'b');
+				//printlist(head_a,head_b);
+			}
+			else if ((*head_a)->val > minb && ((*head_a)->val < maxb))
 			{
 				current = *head_b;
 				nextmin = minb;
@@ -309,14 +446,14 @@ void	sort_medium(t_stack **head_a,t_stack **head_b, int size)
 						nextmin = current->val;
 					current = current->next;
 				}
-				ft_printf("Nextmin = %d \n", nextmin);
+				//ft_printf("Nextmin = %d \n", nextmin);
 				while ((*head_b)->val != nextmin)
 				{
 					shiftup(head_b,'b');
-					printlist(head_a,head_b);
+					//printlist(head_a,head_b);
 				}
 				push(head_a,head_b,'b');
-				printlist(head_a,head_b);
+				//printlist(head_a,head_b);
 			}
 		}
 		if (*head_a == NULL)
@@ -324,12 +461,12 @@ void	sort_medium(t_stack **head_a,t_stack **head_b, int size)
 			while ((*head_b)->val != maxb)
 			{
 				shiftup(head_b,'b');
-				printlist(head_a,head_b);
+				//printlist(head_a,head_b);
 			}
 			while (*head_b != NULL)
 			{
 				push(head_b,head_a,'a');
-				printlist(head_a,head_b);
+				//printlist(head_a,head_b);
 			}
 		}
 	}
@@ -398,8 +535,8 @@ int	main(int argc, char **argv)
 		i++;
 	}
 
-	ft_printf("BEGGINING SORTING WITH THIS LIST \n");
-	printlist(&head_a,&head_b);
+	//ft_printf("BEGGINING SORTING WITH THIS LIST \n");
+	//printlist(&head_a,&head_b);
 	if (stacklen(head_a) == 2)
 		sort_two(&head_a);
 	else if (stacklen(head_a) == 3)
@@ -408,7 +545,7 @@ int	main(int argc, char **argv)
 		sort_small(&head_a, &head_b, stacklen(head_a));
 	else
 		sort_medium(&head_a,&head_b, stacklen(head_a));
-	ft_printf("FINISHED SORTING LIST GRATZ \n");
-	printlist(&head_a,&head_b);
+	//ft_printf("FINISHED SORTING LIST GRATZ \n");
+	//printlist(&head_a,&head_b);
 	return (0);
 }
